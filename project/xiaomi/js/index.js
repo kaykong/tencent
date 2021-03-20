e6706c0919eea71a3ce1abdbc9318f90776f7311
kongkk <kongk1996@qq.com>
2021-03-20 22:36:42 +0800

    小米商城 关键字列表

--- project/xiaomi/js/index.js
+++ project/xiaomi/js/index.js
@@ -1,43 +1,23 @@
 window.onload = function () {
 
+    let headerSearch = document.getElementsByClassName("header-search")[0];
+    let keyWords = headerSearch.getElementsByClassName("search-key-words")[0];
+    /*let i = 0;
+    keyWords.onmouseout = function() {
+      console.log("离开keyWordsList"+ i++);
+    };*/
+
+
     let headerNavLiList = document.getElementsByClassName("Js-header-nav-li");
-
     for (let i = 0; i < headerNavLiList.length; i++) {
-        //给每一个li绑定hover事件
         headerNavLiList[i].onmouseover = function () {
-            // alert("hhh")
-            let headerNavMenu = document.getElementsByClassName("header-nav-menu")[0];
-            headerNavMenu.style.height = "200px";
-            headerNavMenu.style.borderTop = "1px solid rgb(224, 224, 224)";
-            headerNavMenu.style.boxShadow = "0 5px 3px rgba(0, 0, 0, .2)";
-            console.log("1")
-
-        };
-
-        headerNavLiList[i].onmouseout = function () {
-            // alert("hhh")
-            let headerNavMenu = document.getElementsByClassName("header-nav-menu")[0];
-            headerNavMenu.style.height = "0px";
-            headerNavMenu.style.borderTop = "none";
-            headerNavMenu.style.boxShadow = "none";
+            //输入框失去焦点(关键字列表隐藏)
+            document.getElementsByClassName("search-txt")[0].blur();
         };
     }
 
-    let headerNavMenu = document.getElementsByClassName("header-nav-menu")[0];
-    headerNavMenu.onmouseover = function () {
-        this.style.height = "200px";
-        this.style.borderTop = "1px solid rgb(224, 224, 224)";
-        this.style.boxShadow = "0 5px 3px rgba(0, 0, 0, .2)";
-    };
-    headerNavMenu.onmouseout = function () {
-        this.style.height = "0px";
-        this.style.height = "0px";
-        this.style.borderTop = "none";
-        this.style.boxShadow = "none";
-    };
 
 
-    console.log(headerNavLiList)
 
 
 };
