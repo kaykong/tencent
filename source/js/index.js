8773d03405c5baa977031d2abc2b5186ac4988d7
kongkk <kongk1996@qq.com>
2021-03-27 14:17:16 +0800

    首页添加评论区

---
+++ source/js/index.js
@@ -0,0 +1,13 @@
+
+const imgListLength = 21;
+
+window.onload = function () {
+
+    let imgListDiv = document.getElementsByClassName("imgList")[0];
+
+    let imgListInnerHtml = "";
+    for (let i = 1; i <= imgListLength; i++) {
+
+    }
+
+};
