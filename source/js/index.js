a73c9a67c2bc83fdc502973dcd29295d84d4a8a3
kongkk <kongk1996@qq.com>
2021-03-27 19:26:58 +0800

    首页添加评论区 list 插入数据库

--- source/js/index.js
+++ source/js/index.js
@@ -19,11 +19,15 @@ var Ajax = {
     },
 
     // data应为'a=a1&b=b1'这种字符串格式，在jq里如果data为对象会自动将对象转成这种字符串格式
+    // data 为json格式
     post: function (url, data, callback) {
+
+        data = JSON.stringify(data);
+
         var xhr = new XMLHttpRequest();
         xhr.open('POST', url, false);
         // 添加http头，发送信息至服务器时内容编码类型
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.onreadystatechange = function () {
             if (xhr.readyState == 4) {
                 if (xhr.status == 200 || xhr.status == 304) {
@@ -36,9 +40,83 @@ var Ajax = {
     }
 };
 
+var urlEncode = function (param, key, encode) {
+    if(param==null) return '';
+    var paramStr = '';
+    var t = typeof (param);
+    if (t == 'string' || t == 'number' || t == 'boolean') {
+        paramStr += '&' + key + '=' + ((encode==null||encode) ? encodeURIComponent(param) : param);
+    } else {
+        for (var i in param) {
+            var k = key == null ? i : key + (param instanceof Array ? '[' + i + ']' : '.' + i);
+            paramStr += urlEncode(param[i], k, encode);
+        }
+    }
+    return paramStr;
+};
+
+
 
 function showMessages(data) {
-    console.log(data);
+
+    data = JSON.parse(data);
+    console.log("showMessages1", data);
+    console.log("showMessages2", data.respCode);
+    console.log("showMessages3", data.body);
+    if (data && data.respCode == 200 && data.body) {
+        //调用成功
+        let msgListInnerHtml = "";
+        for (let i = 0; i < data.body.length; i++) {
+            let item = data.body[i];
+
+            let newMsg = document.createElement("div");
+            newMsg.className = "msg-item";
+            newMsg.innerHTML =
+                "<div class=\"header\">\n" +
+                "    <div class=\"icon\">\n" +
+                "        <img class=\"imgOn\" src=\"" + item.imgUrl + "\" alt=\"\" width=\"26\" height=\"26\">\n" +
+                "    </div>\n" +
+                "    <div class=\"nickname\">\n" +
+                "        " + item.name + "\n" +
+                "    </div>\n" +
+                "    <div class=\"time\">\n" +
+                "        " + item.createTime + "\n" +
+                "    </div>\n" +
+                "</div>\n" +
+                "<div class=\"content\">\n" +
+                "    " + item.message + "\n" +
+                "</div>";
+            msgListInnerHtml += newMsg.outerHTML;
+        }
+        console.log("msgListInnerHtml", msgListInnerHtml);
+
+        let msgListDiv = document.getElementsByClassName("message-list")[0];
+        msgListDiv.innerHTML = msgListInnerHtml;
+    }
+
+
+
+}
+
+function getNowTime() {
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+
+    function getFull(data) {
+        if (data < 10) {
+            data = "0" + data;
+        }
+        return data;
+    }
+
+    let time = year + "-" + getFull(month) + "-" + getFull(day) + " " + getFull(hours) + ":" + getFull(minutes) + ":" + getFull(seconds);
+    return time;
 }
 
 window.onload = function () {
@@ -58,6 +136,71 @@ window.onload = function () {
     }*/
 
     let addBtn = document.getElementsByClassName("addBtn")[0];
+
+    addBtn.onclick = function () {
+
+        //获取昵称, 获取content
+
+        let nickName = document.getElementById("nickname-input").value;
+        let content = document.getElementById("content-input").value;
+        let nowTime = getNowTime();
+        let imgUrl = "";
+        if (imgIndex < 10) {
+            imgUrl = "./source/img/msg/img00" + imgIndex + ".jpg";
+        } else {
+            imgUrl = "./source/img/msg/img0" + imgIndex + ".jpg";
+        }
+        console.log(nickName, content);
+
+        if (nickName == "") {
+            alert("请填写昵称");
+            return;
+        }
+
+        if (content == "") {
+            alert("请填写留言");
+            return;
+        }
+
+
+        let firstMsgItem = document.getElementsByClassName("msg-item")[0];
+        let newMsg = document.createElement("div");
+        newMsg.className = "msg-item";
+        newMsg.innerHTML =
+            "<div class=\"header\">\n" +
+            "    <div class=\"icon\">\n" +
+            "        <img class=\"imgOn\" src=\"" + imgUrl + "\" alt=\"\" width=\"26\" height=\"26\">\n" +
+            "    </div>\n" +
+            "    <div class=\"nickname\">\n" +
+            "        " + nickName + "\n" +
+            "    </div>\n" +
+            "    <div class=\"time\">\n" +
+            "        " + nowTime + "\n" +
+            "    </div>\n" +
+            "</div>\n" +
+            "<div class=\"content\">\n" +
+            "    " + content + "\n" +
+            "</div>";
+
+        // firstMsgItem.parentNode.firstElementChild
+        //在开头插入节点
+        firstMsgItem.parentNode.insertBefore(newMsg, firstMsgItem);
+
+        let data = {
+            "name": nickName,
+            "message": content,
+            "createTime": nowTime,
+            "imgUrl": imgUrl
+        };
+
+        Ajax.post(
+            "https://service-6qcrvxv3-1305383279.sh.apigw.tencentcs.com/release/mongoDB?methodName=insertOneMessage",
+            data,
+            function (data) {
+                console.log("Ajax.post", data);
+            });
+
+    };
     /*addBtn.addEventListener("touchstart", function () {
         this.className = "addBtn addBtnMobile";
     }, false);
