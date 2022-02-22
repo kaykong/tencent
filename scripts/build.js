f03496ecbbe1557ab5c7e1f806e075e2977726c2
kongkk <kongk1996@qq.com>
2022-02-22 17:19:37 +0800

    build.js修改, 方便编译之后直接部署在文件夹里

--- scripts/build.js
+++ scripts/build.js
@@ -3,6 +3,7 @@
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
+process.env.PUBLIC_URL = '/item-list'; //如果网页不是部署成test.com, 而是 test.com/item-list
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
